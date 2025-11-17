f38a5a245d03032921f36b825a18fd2b373ba2dd
Jingzhe0 <sarthak.956969@gmail.com>
2025-11-17 16:37:20 +0530

    add sound and siri wave

---
+++ engine/features.py
@@ -0,0 +1,7 @@
+from playsound import playsound
+
+
+#playing assistand sound function
+def playAssistantSound():
+    music_dir="www\\assests\\audio\\page-shuffle-transition-429869.mp3"
+    playsound(music_dir)
