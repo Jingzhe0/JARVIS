d0cef59419022da9a31df862a25d8e3a48e34898
Jingzhe0 <sarthak.956969@gmail.com>
2025-11-23 17:48:53 +0530

    add whatsapp feature

--- engine/helper.py
+++ engine/helper.py
@@ -10,4 +10,14 @@ def extract_yt_term(command):
     return match.group(1) if match else None 
 
 
+def remove_words(input_string, words_to_remove):
+    # split input string to words
+    words= input_string.split()
 
+    # removbe unwanted words
+    filtered_words= [word for word in words if word.lower() not in words_to_remove]
+
+    # join words in string
+    result_string= ' '.join(filtered_words)
+
+    return result_string
